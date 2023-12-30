7cd1276ad7dcf627877f72bde34f469b06275421
Sean Outram <outramsean0@gmail.com>
2023-12-30 09:29:55 +0000

    A routes change that got lost

--- web/config/routes.rb
+++ web/config/routes.rb
@@ -6,11 +6,6 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  scope "user" do
-    get "login", to: "user#login", as: :user_login
-    post "send_code", to: "user#send_code", as: :user_auth_send_code
-    get "authenticate/:token", to: "user#authenticate", as: :user_auth_authenticate
-  end
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   # root "posts#index"
 end
