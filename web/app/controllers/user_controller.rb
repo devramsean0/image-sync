c3c8aa8ca499f8c2610d0334c949f6d01e61d002
Devramsean0 <81807361+devramsean0@users.noreply.github.com>
2024-01-05 17:55:49 +0000

    A sort of built homepage for web

--- web/app/controllers/user_controller.rb
+++ web/app/controllers/user_controller.rb
@@ -2,5 +2,6 @@ class UserController < ApplicationController
     skip_before_action :authenticate_user!, only: :index
     def index
         redirect_to auth_path unless user_signed_in?
+        @collections = current_user.collections
     end
 end
