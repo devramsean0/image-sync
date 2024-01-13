75316d87fb0bcfdcdab272eedc4c6cbcf7b9e918
Devramsean0 <81807361+devramsean0@users.noreply.github.com>
2024-01-13 22:40:54 +0000

    Barebones desktop-lite application

--- desktop-lite/src-tauri/src/main.rs
+++ desktop-lite/src-tauri/src/main.rs
@@ -1,15 +1,13 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use rust_lib::auth;
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
-
-fn main() {
+#[tokio::main]
+async fn main() {
+    let state = auth::init().await;
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![auth::authenticate])
+        .manage(state)
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- desktop-lite/src/App.tsx
+++ desktop-lite/src/App.tsx
@@ -1,6 +1,11 @@
+import { invoke } from "@tauri-apps/api/tauri";
+
 function App() {
   return (
-    <h1 class="text-4xl">Image Sync</h1>
+    <>
+      <h1 class="text-4xl">Image Sync</h1>
+      <button onClick={() => invoke("authenticate")}>Login</button>
+    </>
   );
 }
 
